24fbf294d01ca7cc4a8cdbb15f9fc5fabd4448b1
nadium <philinh.ngo@gmail.com>
2014-04-01 15:24:53 +0200

    Ordi psd + < 960px

--- js/script.js
+++ js/script.js
@@ -1,8 +1,23 @@
 $(document).ready(function() {
-
-	$('#menu-guy a').hover(function() {
-		$('#circle-guy').trigger('mouseOver');
-	});
-
-
-})
+	$('#menu-guy a').hover(
+		function() {
+			$('#circle-guy').addClass('hover');
+		},function(){
+			$('#circle-guy').removeClass('hover');
+		}
+	);
+	$('#menu-stuff a').hover(
+		function() {
+			$('#circle-stuff').addClass('hover');
+		},function(){
+			$('#circle-stuff').removeClass('hover');
+		}
+	);
+	$('#menu-cat a').hover(
+		function() {
+			$('#circle-cat').addClass('hover');
+		},function(){
+			$('#circle-cat').removeClass('hover');
+		}
+	);
+});
